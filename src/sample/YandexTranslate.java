fb317713263a435cf5626bee9db42bbc163dff12
EGOR SEVASTIANOV <egor.egeg@yandex.ru>
2019-06-19 17:21:33 +0300

    first

---
+++ src/sample/YandexTranslate.java
@@ -0,0 +1,38 @@
+package sample;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Scanner;
+
+public class YandexTranslate {
+    private static int i = 0;
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter line");
+        String line = scanner.nextLine();
+        System.out.println(translate("en", line));
+    }
+
+    private static String translate(String lang, String input) throws IOException {
+        String urlStr = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20150627T071448Z.117dacaac1e63b79.6b1b4bb84635161fcd400dace9fb2220d6f344ef";
+        URL urlObj = new URL(urlStr);
+        HttpsURLConnection connection = (HttpsURLConnection)urlObj.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        DataOutputStream dataOutputStream = new DataOutputStream(connection.getOutputStream());
+        dataOutputStream.writeBytes("text=" + URLEncoder.encode(input, "UTF-8") + "&lang=" + lang);
+
+        InputStream response = connection.getInputStream();
+        String json = new java.util.Scanner(response).nextLine();
+        int start = json.indexOf("[");
+        int end = json.indexOf("]");
+        String translated = json.substring(start + 2, end - 1);
+        i++;
+        if (translated.equals(input) && i < 2) {
+            return translate("ru", input);
+        } else return translated;
+    }
+}
